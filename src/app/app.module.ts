88dcfc22acd2afa63d1cac9f08fe18ec53175a8a
Anthony Pugliese <anthony.pugliese@kaiser-x.com>
2019-03-08 10:55:20 +0100

    added basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule, MatButtonModule } from '@angular/material';
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatListModule,
+  MatIconModule
+} from '@angular/material';
 import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
@@ -14,7 +21,11 @@ import { HomeComponent } from './pages/home/home.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
